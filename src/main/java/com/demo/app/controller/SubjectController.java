3bc5f35f2f6d4acc71e47d25d469a10662f27d23
HuuKhiem <huukhiem001@gmail.com>
2023-06-19 14:20:41 +0700

    cors 3

--- src/main/java/com/demo/app/controller/SubjectController.java
+++ src/main/java/com/demo/app/controller/SubjectController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/subject")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 @RequiredArgsConstructor
 public class SubjectController {
 
